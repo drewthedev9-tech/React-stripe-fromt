2e8fba21cdc078febfc227b2569974db9a8677ff
drewthedev9@gmail.com <45505005+drewthedev9-tech@users.noreply.github.com>
2020-06-18 19:16:22 -0400

    added changes button

--- src/button/button.jsx
+++ src/button/button.jsx
@@ -4,7 +4,7 @@ import button from "./button.css"
 const Button = () => {
     return ( 
         <div className="button-flex">
-        <button className="button" type="submit">
+        <button className="button">
        Confirm Order
        </button>
         </div>
